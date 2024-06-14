1aef2951f8d8dd2586f1ae61806d337f0b09b772
Ibrahim Ahmed Shaikh <108570801+dumbibu@users.noreply.github.com>
2024-06-14 06:42:20 +0500

    Add files via upload

--- nodesocketcode.ino
+++ nodesocketcode.ino
@@ -2,40 +2,36 @@
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
 
-// WiFi credentials
-const char* ssid = "CONNECT(IBRAHIM)2.4_plus";
-const char* password = "ibrahim999@";
 
-// WebSocket server URL
-const char* websockets_server_host = "farm.dijinx.com"; // Server URL
-const uint16_t websockets_server_port = 443; // Server port for HTTPS
+const char* ssid = "WIFI";
+const char* password = "PASS";
+
+
+const char* websockets_server_host = "farm.dijinx.com"; 
+const uint16_t websockets_server_port = 443; 
 
 WebSocketsClient webSocket;
 
-// Pin for the relay
-const int relayPin = D1; // Assuming the relay is connected to GPIO pin D2
+
+const int relayPin = D1; 
 bool relayActivated = false;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
     case WStype_DISCONNECTED:
       Serial.printf("[WSc] Disconnected!\n");
-      // Immediate reconnection
       webSocket.beginSSL(websockets_server_host, websockets_server_port, "/ws");
       break;
     case WStype_CONNECTED:
       Serial.printf("[WSc] Connected to url: %s\n", payload);
-      // Send JSON message to the WebSocket server
-      webSocket.sendTXT("{\"arduino_id\":\"6O6ZEA\"}");
+      webSocket.sendTXT("{\"arduino_id\":\"YOURSENSORID\"}");
       break;
     case WStype_TEXT:
       {
         Serial.printf("[WSc] Received text: %s\n", payload);
-        // Parse JSON payload
         DynamicJsonDocument doc(256);
         deserializeJson(doc, payload);
         int duration = doc["payload"]["duration"];
-        // Activate relay for the specified duration
         activateRelay(duration);
       }
       break;
@@ -48,9 +44,9 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 void activateRelay(int durationInMinutes) {
   if (!relayActivated) {
     Serial.printf("Activating relay for %d minutes\n", durationInMinutes);
-    pinMode(relayPin, HIGH); // Turn on the relay
+    pinMode(relayPin, HIGH); 
     relayActivated = true;
-    delay(durationInMinutes * 60000); // Convert minutes to milliseconds
+    delay(durationInMinutes * 60000); 
     pinMode(relayPin, LOW);
     
     Serial.println("Relay deactivated");
@@ -63,10 +59,8 @@ void activateRelay(int durationInMinutes) {
 }
 
 void setup() {
-  // Start Serial
   Serial.begin(115200);
 
-  // Connect to WiFi
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -83,20 +77,14 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Set relay pin as output
   pinMode(relayPin, LOW);
 
-  // WebSocket connection
   webSocket.beginSSL(websockets_server_host, websockets_server_port, "/ws");
   
-  // Event handler
   webSocket.onEvent(webSocketEvent);
 
-  // Set timeouts
-  webSocket.setReconnectInterval(5000); // Try to reconnect every 5 seconds
-}
+  webSocket.setReconnectInterval(5000); 
 
 void loop() {
-  // WebSocket loop
   webSocket.loop();
 }
